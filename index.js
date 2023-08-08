0c348d995d60b43ee3608e37541be9c70836a016
AchieBee <belza.achieng@student.moringaschool.combelza.achieng@student.moringaschool.com>
2023-08-08 13:49:40 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+//FUNCTION isPalindrome(str)
+  //  cleanStr = removeNonLettersAndLowercase(str)
+    //reversedStr = reverse(cleanStr)
+    //RETURN cleanStr EQUALS reversedStr
+//END FUNCTION
+
+//FUNCTION removeNonLettersAndLowercase(str)
+  //  cleanStr = ""
+    //FOR each character in str
+      //  IF character IS a lowercase letter
+        //    Append character to cleanStr
+    //RETURN cleanStr
+//END FUNCTION
+
+//FUNCTION reverse(str)
+  //  reversedStr = ""
+    //FOR i from length of str - 1 down to 0
+      //  Append str[i] to reversedStr
+    //RETURN reversedStr
+//END FUNCTION
+
+function isPalindrome(str) {
+  const cleanStr = removeNonLettersAndLowercase(str);
+  const reversedStr = reverse(cleanStr);
+  return cleanStr === reversedStr;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+function removeNonLettersAndLowercase(str) {
+  let cleanStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (/[a-z]/.test(str[i])) {
+      cleanStr += str[i];
+    }
+  }
+  return cleanStr.toLowerCase();
 }
 
-module.exports = isPalindrome;
+function reverse(str) {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+}
+
+// Test cases
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("a"));  
+console.log(isPalindrome("robot")); 
+console.log(isPalindrome("ab"));
